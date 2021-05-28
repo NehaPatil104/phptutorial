42b9fc02c784f81e25c831e80346ab3f80b4ca2e
NehaPatil104 <71131714+NehaPatil104@users.noreply.github.com>
2021-05-28 13:34:58 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;300;400;500;600&display=swap" rel="stylesheet">
+      <!-- font awesome link -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous">
+    <title>PHP Tutorial</title>
+  </head>
+  <body>
+    <section class="container-fluid p-0">
+      <div class="row" style="height: 100vh;">
+          <div class="col-4 text-center" style="margin-top: 15%;">
+              <h1 class="display-3 clr">PHP TUTORIAL</h1>
+              <hr style="width: 450px; border: 1px solid black; margin: auto;" class="clr-secondary">
+              <h2 class="display-6 clr mt-5">Neha Sadanand Patil 186140</h2>
+              <button class="btn btn-primary btn-design"><a href="tutorial/introduction.php">Start Tutorial &nbsp;<i class="far fa-hand-point-right" style="font-size: 25px;"></i> </a></button>
+          </div>
+          <div class="col-8 p-0">
+              <img src="images/backimg.png" style="width: 100%; height: 100%;">
+          </div>
+      </div>
+    </section>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+
+  </body>
+</html>
---
+++ top.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" type="text/css" href="../css/tutorial.css">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;300;400;500;600&display=swap" rel="stylesheet">
+
+    <title>PHP Tutorial</title>
+  </head>
+  <body>
+    <section>
+      <div class="row">
+        <div class="col-2 sidenav pe-0">
+          <h4 class="p-3 text-white"><a href="../index.php">PHP Tutorial</a></h4>
+
+           <a href="introduction.php">Introduction</a>
+          <a href="operators.php">PHP Operators</a>
+          <a href="#">Link</a>
+          <a href="#">Link</a>
+        </div>
